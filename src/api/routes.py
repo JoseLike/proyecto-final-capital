0a375a19c7e322f5effe2a5b4e3b8474db6228f2
Sam0294 <sam_f94@hotmail.com>
2022-05-16 20:58:11 +0000

    ruta register

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,6 +8,22 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-    return jsonify(response_body), 200
+@api.route('/register', methods=["POST"])
+def create_user():
+    body_email = request.json.get("email")
+    body_password = request.json.get("password")
+    body_username = request.json.get("username")
+    body_pais = request.json.get("pais")
+    body_nombre = request.json.get("nombre")
+    """ pendiente de saber como es el is_premium??? """
+    if body_email and body_password:
+        new_user = User(email = body_email, password = body_password, is_active=False)
+        """ validacion de cuenta existente??? """
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"created":True, "user":new_user.serialize()}), 200
+    else:
+        return jsonify({"created":False, "msg":"Lack of Info"}), 400
+
+
+
