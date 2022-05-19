c80bde476536527453363e865c92a62a8546a60f
JoseLike <joseluisgil94@gmail.com>
2022-05-19 17:27:37 +0000

    añadido dropdown de regiones y cambiado estilo

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -18,6 +18,9 @@ export const Register = () => {
   const [notTouched2, setnotTouched2] = useState(
     "btt-account-type col-4 text-center shadow m-2 rounded"
   );
+  const [classInputCompany, setclassInputCompany] = useState(
+    "form-check-input mt-0 me-2 "
+  );
 
   const [datos, setDatos] = useState({
     user_type: "",
@@ -26,8 +29,7 @@ export const Register = () => {
     email: "",
     country: "",
     password: "",
-    company: false,
-    freelance: false,
+    inversor_type: "",
     category: "",
     acepted_conditions: false,
     is_premium: false,
@@ -53,7 +55,7 @@ export const Register = () => {
     }
   };
 
-  const sendUserInfo = async () => {
+  const sendNewUser = async () => {
     if (datos.email != null && datos.password.trim() != "") {
       const response = await fetch("", {
         method: "POST",
@@ -61,12 +63,7 @@ export const Register = () => {
         body: JSON.stringify(datos),
       });
       const data = await response.json();
-      localStorage.setItem("token", data.token);
-      if (data.logged == true) {
-        actions.logTrue();
-      }
       console.log(data);
-      //navigate.push("/home");
     } else {
       return alert("Falta informacion");
     }
@@ -162,11 +159,11 @@ export const Register = () => {
             <div className="d-flex justify-content-center pt-4">
               <div className="d-flex ">
                 <input
-                  className="form-check-input mt-0 me-2"
+                  className={classInputCompany}
                   type="checkbox"
                   checked={datos.company}
                   onChange={() => {
-                    setDatos({ ...datos, company: !datos.company });
+                    setDatos({ ...datos, inversor_type: "company" });
                   }}
                   aria-label="Checkbox for company"
                   name="company"
@@ -175,11 +172,15 @@ export const Register = () => {
               </div>
               <div className="d-flex input-basic-register">
                 <input
-                  className="form-check-input mt-0 me-2"
+                  className={
+                    datos.company != true
+                      ? "form-check-input mt-0 me-2"
+                      : "form-check-input mt-0 me-2"
+                  }
                   type="checkbox"
                   checked={datos.freelance}
                   onChange={() => {
-                    setDatos({ ...datos, freelance: !datos.freelance });
+                    setDatos({ ...datos, inversor_type: "freelance" });
                   }}
                   aria-label="Checkbox for following text input"
                 />
@@ -189,85 +190,98 @@ export const Register = () => {
               </div>
             </div>
             <div className="justify-content-center">
-              <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="inputGroup-sizing-sm">
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">
                     Name
                   </span>
                 </div>
                 <input
                   type="text"
-                  class="form-control"
+                  name="name"
+                  className="form-control"
                   aria-label="Small"
                   onChange={handleInputChange}
                   aria-describedby="inputGroup-sizing-sm"
                 />
               </div>
-              <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="inputGroup-sizing-sm">
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">
                     Last Name
                   </span>
                 </div>
                 <input
                   type="text"
-                  class="form-control"
+                  name="last_name"
+                  className="form-control"
                   aria-label="Small"
                   onChange={handleInputChange}
                   aria-describedby="inputGroup-sizing-sm"
                 />
               </div>
-              <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="inputGroup-sizing-sm">
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">
                     Email
                   </span>
                 </div>
                 <input
                   type="text"
-                  class="form-control"
+                  name="email"
+                  className="form-control"
                   aria-label="Small"
                   onChange={handleInputChange}
                   aria-describedby="inputGroup-sizing-sm"
                 />
               </div>
-              <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="inputGroup-sizing-sm">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <label
+                    className="input-group-text"
+                    htmlFor="inputGroupSelect01"
+                  >
                     Country
-                  </span>
+                  </label>
                 </div>
-                <input
-                  type="text"
-                  class="form-control"
-                  aria-label="Small"
+                <select
+                  className="custom-select"
+                  name="country"
+                  id="inputGroupSelect01"
                   onChange={handleInputChange}
-                  aria-describedby="inputGroup-sizing-sm"
-                />
+                >
+                  <option value>Choose your Region</option>
+                  <option value="spain">Spain</option>
+                  <option value="venezuela">Venezuela</option>
+                  <option value="europe">Europe</option>
+                  <option value="uk">U.K</option>
+                  <option value="usa">U.S.A</option>
+                </select>
               </div>
-              <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="inputGroup-sizing-sm">
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">
                     Password
                   </span>
                 </div>
                 <input
                   type="text"
-                  class="form-control"
+                  name="password"
+                  className=" form-control"
                   aria-label="Small"
                   onChange={handleInputChange}
                   aria-describedby="inputGroup-sizing-sm"
                 />
               </div>
-              <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="inputGroup-sizing-sm">
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">
                     Repeat Password
                   </span>
                 </div>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   aria-label="Small"
                   onChange={handleInputChange}
                   aria-describedby="inputGroup-sizing-sm"
@@ -295,8 +309,8 @@ export const Register = () => {
                   type="button"
                   className={
                     datos.acepted_conditions != true
-                      ? "col-3 btn btn-outline-success disabled"
-                      : "col-3 btn btn-outline-success"
+                      ? "col-3 btn btn-outline-primary disabled"
+                      : "col-3 btn btn-primary"
                   }
                   onClick={() => verify()}
                   aria-disabled="false"
