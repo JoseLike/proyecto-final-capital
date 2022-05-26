1f3abfdf6ec2a964cb9ea1e37df88153c0f3a7ef
Sam0294 <sam_f94@hotmail.com>
2022-05-26 17:09:14 +0000

    actualizo inputs

---
+++ src/front/js/pages/BuscarProyecto.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/home.css";
+
+export const BuscarProyecto = () => {
+  return (
+    <div>
+      <button type="button" className="btn btn-success">
+        Buscar
+      </button>
+    </div>
+  );
+};
